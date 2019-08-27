b29d00f26b0bf4e999467e8bc829aa9c1c206ef2
TimKnupfer <timknupfer@t-online.de>
2019-08-27 17:28:57 +0200

    1 file modified, 1 added

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fermat-test.component.css']
 })
 export class FermatTestComponent implements OnInit {
-
+Versuchskaninchen: number = 13;
+Anzahl: number=5;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+Ergebnis: "N/A"
+{{Ergebnis}} 
