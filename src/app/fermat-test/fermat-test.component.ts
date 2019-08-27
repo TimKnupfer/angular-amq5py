26c84c480aa4ed992d2ec2d3883c581f4319cd62
TimKnupfer <timknupfer@t-online.de>
2019-08-27 18:25:12 +0200

    2 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -6,11 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fermat-test.component.css']
 })
 export class FermatTestComponent implements OnInit {
+
 Versuchskaninchen: number = 13;
 Anzahl: number=5;
 Ergebnis="N/A"
+
   constructor() { }
 
+fermattest()   {
+this.Ergebnis = "starte Fermat-Test der Zahl" +this.Versuchskaninchen + "...";
+
+ let zeuge = 2;
+ // Berchne zeuge^(Versuchskaninchen-1) modulo Versuchskaninchen
+ let Zwischenergebnis = 1;
+ for(let i=0; i <this.Versuchskaninchen-1; i++) {
+ Zwischenergebnis = Zwischenergebnis * zeuge
+ }
+Zwischenergebnis = Zwischenergebnis % this.Versuchskaninchen;
+if(Zwischenergebnis===1){
+   this.Ergebnis += "zeuge " + zeuge+ " Ok...";
+}else{
+  this.Ergebnis += "Zeuge " + zeuge + " Nicht Ok!";
+}
+
+}
   ngOnInit() {
   }
 
