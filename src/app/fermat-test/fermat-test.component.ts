3c06a5d137d3fec3dc30f7ce36fdca3fc22d6bc6
TimKnupfer <timknupfer@t-online.de>
2019-08-28 09:22:15 +0200

    1 file modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -7,29 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FermatTestComponent implements OnInit {
 
-Versuchskaninchen: number = 13;
-Anzahl: number=5;
-Ergebnis="N/A"
+  Versuchskaninchen: number = 13;
+  Anzahl: number = 5;
+  Ergebnis = "N/A"
 
   constructor() { }
 
-fermattest()   {
-this.Ergebnis = "starte Fermat-Test der Zahl" +this.Versuchskaninchen + "...";
+  fermattest() {
+    this.Ergebnis = "starte Fermat-Test der Zahl" + this.Versuchskaninchen + "...";
 
- let zeuge = 2;
- // Berchne zeuge^(Versuchskaninchen-1) modulo Versuchskaninchen
- let Zwischenergebnis = 1;
- for(let i=0; i <this.Versuchskaninchen-1; i++) {
- Zwischenergebnis = Zwischenergebnis * zeuge
- }
-Zwischenergebnis = Zwischenergebnis % this.Versuchskaninchen;
-if(Zwischenergebnis===1){
-   this.Ergebnis += "zeuge " + zeuge+ " Ok...";
-}else{
-  this.Ergebnis += "Zeuge " + zeuge + " Nicht Ok!";
-}
+    for (let zeuge = 2; zeuge < this.Anzahl + 2; zeuge++) {
 
-}
+
+      // Berchne zeuge^(Versuchskaninchen-1) modulo Versuchskaninchen
+      let Zwischenergebnis = 1;
+      for (let i = 0; i < this.Versuchskaninchen - 1; i++) {
+        Zwischenergebnis = Zwischenergebnis * zeuge
+      }
+      Zwischenergebnis = Zwischenergebnis % this.Versuchskaninchen;
+      if (Zwischenergebnis === 1) {
+        this.Ergebnis += "zeuge " + zeuge + " Ok...";
+      } else {
+        this.Ergebnis += "Zeuge " + zeuge + " Nicht Ok!";
+      }
+    }
+  }
   ngOnInit() {
   }
 
